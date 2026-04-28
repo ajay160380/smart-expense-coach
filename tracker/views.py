8ee0960747e93bc9594c0f4b1b374ce9c998ce75
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-04-28 22:08:14 +0530

    Enhanced dashboard UI with insight section

--- tracker/views.py
+++ tracker/views.py
@@ -13,10 +13,25 @@ def dashboard(request):
         else:
             category_data[exp.category] = exp.amount
 
+    # 🔥 SMART INSIGHT LOGIC
+    insight = ""
+
+    if total > 0:
+        for category, amount in category_data.items():
+            percent = (amount / total) * 100
+
+            if percent > 50:
+                insight = f"You are spending too much on {category} ({percent:.1f}%) ⚠️"
+                break
+
+    if insight == "":
+        insight = "Your spending looks balanced 👍"
+
     context = {
         'total': total,
         'category_data': category_data,
-        'expenses': expenses
+        'expenses': expenses,
+        'insight': insight   # 👈 new
     }
 
     return render(request, 'tracker/dashboard.html', context)
