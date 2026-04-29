33988c44ab6c38ef971c92c479f2c312ce525169
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-04-29 21:56:21 +0530

    feat: added User Authentication, Login/Register UI, and private user dashboards

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views  
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'), # Main page ab dashboard hai
+    path('', views.dashboard, name='dashboard'),
     path('add/', views.add_expense, name='add_expense'),
     path('edit/<int:pk>/', views.edit_expense, name='edit_expense'),
     path('delete/<int:pk>/', views.delete_expense, name='delete_expense'),
+
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
