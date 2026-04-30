692ae4232118dd60990a3b6b16534cc28a10aa9f
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-04-30 23:10:49 +0530

    feat: add auto-deduct subscription tracker and upcoming bills UI

--- tracker/urls.py
+++ tracker/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('add-sub/', views.add_subscription, name='add_subscription'),
 ]
