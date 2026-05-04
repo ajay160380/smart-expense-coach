5328d05c653d0ef05824dbc2c1cb74525a728b50
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-05-04 10:35:17 +0530

    feat: create CustomRegistrationForm and API Serializer for phone numbers

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Expense, Subscription 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Expense, Subscription, UserProfile 
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
@@ -10,3 +12,24 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['name', 'amount', 'category', 'next_billing_date']
+
+class CustomRegistrationForm(UserCreationForm):
+    phone_number = forms.CharField(
+        max_length=15,
+        required=True,
+        help_text="Apna WhatsApp number daalein (e.g., 917905398965)"
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('phone_number',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            UserProfile.objects.create(
+                user=user,
+                phone_number=self.cleaned_data['phone_number']
+            )
+        return user
