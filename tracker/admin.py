e1b899b7f74fb4b4e3198a26977cf0817ea754bd
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-06-18 21:57:08 +0530

    feat: add models for savings goals and expense splits

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Expense, Subscription, UserProfile # UserProfile add kiya
+from .models import Expense, Subscription, UserProfile, SavingsGoal, SplitGroup, SplitExpense, SplitMember
 
 admin.site.register(Expense)
 admin.site.register(Subscription)
 admin.site.register(UserProfile)
+admin.site.register(SavingsGoal)
+admin.site.register(SplitGroup)
+admin.site.register(SplitExpense)
+admin.site.register(SplitMember)
