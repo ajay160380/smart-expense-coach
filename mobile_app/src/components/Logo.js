059d3174aa34d729a106fb3c33f1dcf4d40519a2
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-07-23 16:18:34 +0530

    Fix Logo by using remote image URL to bypass Expo asset resolution

--- mobile_app/src/components/Logo.js
+++ mobile_app/src/components/Logo.js
@@ -11,12 +11,13 @@ export default function Logo({ size = 1, showText = true, circle = true }) {
       
       {/* Wrapper for Icon */}
       <View style={[
-        { width: 220 * s, height: 220 * s, justifyContent: 'center', alignItems: 'center' },
+        { width: 220 * s, height: 220 * s, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' },
         circle ? { backgroundColor: '#FFF', borderRadius: 110 * s, shadowColor: '#000', shadowOffset: { width: 0, height: 10 * s }, shadowOpacity: 0.1, shadowRadius: 20 * s, elevation: 10 } : {}
       ]}>
         <Image 
-          source={require('../../assets/logo.png')} 
-          style={{ width: 180 * s, height: 180 * s, resizeMode: 'contain', borderRadius: 24 * s }} 
+          source={{ uri: 'https://ajay160380-paisa-mitra.hf.space/static/tracker/images/icon.png' }} 
+          resizeMode="contain"
+          style={{ width: 180 * s, height: 180 * s }} 
         />
       </View>
     </View>
